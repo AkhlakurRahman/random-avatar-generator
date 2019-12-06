62ed9089c31d0b39efab9069d24d66c99bc34a00
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-07 01:47:30 +0600

    completed

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,22 +1,62 @@
 <template>
-	<div>
+	<div class="wrapper">
 		<h3>Random Avatar Generator</h3>
 
 		<div>
 			<label for="avatarInput">Input:</label>
-			<input type="text" name="avatar" id="avatarInput" />
+			<input type="text" v-on:input="onInput" id="avatarInput" />
 		</div>
 
-		<div>
+		<div class="output">
 			Output:
+			<div v-html="indenticon"></div>
 		</div>
 	</div>
 </template>
 
 <script>
+import jdenticon from 'jdenticon';
+
 export default {
-	name: 'InputField'
+	name: 'InputField',
+	data() {
+		return {
+			avatar: ''
+		};
+	},
+	computed: {
+		indenticon() {
+			if (this.avatar !== '') {
+				return jdenticon.toSvg(this.avatar, 200);
+			}
+			return null;
+		}
+	},
+	methods: {
+		onInput: function(event) {
+			this.avatar = event.target.value;
+		}
+	}
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.wrapper {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+h3 {
+	margin: 50px 0;
+}
+
+label {
+	margin-right: 10px;
+}
+
+.output {
+	margin-top: 20px;
+}
+</style>
