d9ae3ef8a39169b9ea418fea37f498854262beba
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-06 20:37:40 +0600

    initial commit

---
+++ src/components/InputField.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h3>Random Avatar Generator</h3>
+
+		<div>
+			<label for="avatarInput">Input:</label>
+			<input type="text" name="avatar" id="avatarInput" />
+		</div>
+
+		<div>
+			Output:
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'InputField'
+};
+</script>
+
+<style lang="scss" scoped></style>
